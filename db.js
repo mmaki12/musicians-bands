99239c6d4afd9a415d0af90825c2bef06ff180cd
mmaki12 <mmaki12@hotmail.com>
2023-11-01 14:15:19 +0000

    woow

--- db.js
+++ db.js
@@ -2,6 +2,11 @@ const path = require('path');
 const { Sequelize, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const db = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite") 
+});
+
 
 module.exports = {
     sequelize,
--- index.js
+++ index.js
@@ -2,6 +2,76 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../db');
+
+class Band extends Model {}
+
+Band.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Band',
+  }
+);
+
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../db'); 
+
+class Musician extends Model {}
+
+Musician.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    instrument: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Musician',
+  }
+);
+
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../db');
+
+class Song extends Model {}
+
+Song.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    length: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Song',
+  }
+);
+
 
 
 
